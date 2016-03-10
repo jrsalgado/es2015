3a3ca3af9cd8b093e56b2b3bb61fd928f8ed3dcf
Jayro Salgado <jsalgado@nearsoft.com>
2016-03-10 15:13:14 -0700

    comment comp

--- destructingAssignment/arrayMatching.js
+++ destructingAssignment/arrayMatching.js
@@ -15,6 +15,6 @@ console.log(a,b);
 /**
  * ES5
  */
-var list = [ 1, 2, 3 ];
-var a = list[0], b = list[2];
-var tmp = a; a = b; b = tmp;
+// var list = [ 1, 2, 3 ];
+// var a = list[0], b = list[2];
+// var tmp = a; a = b; b = tmp;
